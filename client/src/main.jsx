9b516e741bc670f0ecf9255d76f118912e3491a6
shantanuk7 <shantanukulkarni365@gmail.com>
2025-08-29 20:19:23 +0530

    frontend for posts page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
-import Router from './router'
+import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <Router />
+      <App />
     </AuthProvider>
   </StrictMode>,
 )
